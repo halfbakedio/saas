654f983a7e46d605d26f18234ec0c5fa420c9ff2
Geoff Johnson <geoff.jay@gmail.com>
2024-05-22 14:10:27 -0700

    feat: add ent

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+
+	"github.com/halfbakedio/saas/ent"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	fmt.Println("do something")
+	client, err := ent.Open("postgres", "host=127.0.0.1 port=5432 user=postgres dbname=saas password=postgres sslmode=disable")
+	if err != nil {
+		log.Fatalf("failed opening connection to postgres: %v", err)
+	}
+	defer client.Close()
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
 }
